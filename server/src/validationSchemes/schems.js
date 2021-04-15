58a94e379b041667c877bf6ae70997088e52aebb
rrev <68103311+rrev4ge@users.noreply.github.com>
2021-04-15 20:04:28 +0300

    bugfix validation

--- server/src/validationSchemes/schems.js
+++ server/src/validationSchemes/schems.js
@@ -34,4 +34,3 @@ module.exports.forgotPassTokenSchem = yup.object().shape({
   password: yup.string().required().min(1),
   url: yup.string().required().matches(/^(?:([a-z0-9+.-]+):\/\/)(?:\S+(?::\S*)?@)?(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/),
 });
-
